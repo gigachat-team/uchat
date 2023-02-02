64e14abc23e369c53b26d26946a8f1fc93562650
fomgleb <fomglib@gmail.com>
2023-02-02 10:57:09 +0300

    Replace all write and read funcs with send and recv

--- utils/src/recievers.c
+++ utils/src/recievers.c
@@ -2,25 +2,25 @@
 
 unsigned char recieve_unsigned_char(int socket) {
     unsigned char recieved_character;
-    read(socket, &recieved_character, sizeof(recieved_character));
+    recv(socket, &recieved_character, sizeof(recieved_character), 0);
     return recieved_character;
 }
 
 uint16_t recieve_unsigned_short(int socket) {
     uint16_t recieved_number;
-    read(socket, &recieved_number, sizeof(recieved_number));
+    recv(socket, &recieved_number, sizeof(recieved_number), 0);
     return ntohs(recieved_number);
 }
 
 uint32_t recieve_unsigned_int(int socket) {
     uint32_t recieved_number;
-    read(socket, &recieved_number, sizeof(recieved_number));
+    recv(socket, &recieved_number, sizeof(recieved_number), 0);
     return ntohl(recieved_number);
 }
 
 char *recieve_string(int socket, int max_string_length) {
     char *recieved_string = malloc(max_string_length);
-    read(socket, recieved_string, max_string_length);
+    recv(socket, recieved_string, max_string_length, 0);
     char *returning_string = mx_strdup(recieved_string);
     free(recieved_string);
     return returning_string;
--- utils/src/senders.c
+++ utils/src/senders.c
@@ -1,7 +1,7 @@
 #include "../utils.h"
 
 void send_unsigned_char(int socket, unsigned char character) {
-    write(socket, &character, sizeof(character));
+    send(socket, &character, sizeof(character), 0);
 }
 
 void send_unsigned_short(int socket, uint16_t number) {
@@ -11,7 +11,7 @@ void send_unsigned_short(int socket, uint16_t number) {
 
 void send_unsigned_int(int socket, uint32_t number) {
     uint32_t converted_number = htonl(number);
-    write(socket, &converted_number, sizeof(converted_number));
+    send(socket, &converted_number, sizeof(converted_number), 0);
 }
 
 void send_string(int socket, char *string) {
