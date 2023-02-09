f87dc6e6b34466b9af71e74fad0e31f69e9fcfc1
fomgleb <fomglib@gmail.com>
2023-02-09 09:48:52 +0300

    Rename database -> db

--- server/src/database/data_checkers.c
+++ server/src/database/data_checkers.c
@@ -1,33 +1,33 @@
 #include "../../server.h"
 
-bool db_users_table_has_login(sqlite3 *database, char *login) {
+bool db_users_table_has_login(sqlite3 *db, char *login) {
     char *sql = NULL;
     asprintf(&sql, "SELECT * FROM "USERS_TABLE" \
                     WHERE "USERS_LOGIN" = '%s'", login
     );
 
-    sqlite3_stmt *statement = db_open_statement(database, sql);
+    sqlite3_stmt *statement = db_open_statement(db, sql);
 
     bool has_login = sqlite3_step(statement) == SQLITE_ROW;
 
     free(sql);
-    db_close_statement(statement, database);
+    db_close_statement(statement, db);
 
     return has_login;
 }
 
-bool db_user_is_in_chat(sqlite3 *database, id_t user_id, id_t chat_id) {
+bool db_user_is_in_chat(sqlite3 *db, id_t user_id, id_t chat_id) {
     char *sql = NULL;
     asprintf(&sql, "SELECT * FROM "MEMBERS_TABLE" \
         WHERE "MEMBERS_USER_ID" = %d AND "MEMBERS_CHAT_ID" = %d", user_id, chat_id
     );
 
-    sqlite3_stmt *statement = db_open_statement(database, sql);
+    sqlite3_stmt *statement = db_open_statement(db, sql);
 
     bool user_is_in_chat = sqlite3_step(statement) == SQLITE_ROW;
 
     free(sql);
-    db_close_statement(statement, database);
+    db_close_statement(statement, db);
 
     return user_is_in_chat;
 }
