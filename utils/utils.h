b68bc5794c69bc6857fb03a3bfcbfa7ca348d1e5
fomgleb <fomglib@gmail.com>
2023-04-08 09:46:24 +0300

    Add functions for working with list

--- utils/utils.h
+++ utils/utils.h
@@ -113,6 +113,23 @@ typedef enum e_state_code {
     SUCH_USER_IS_ALREADY_IN_CHAT
 } t_state_code;
 
+typedef struct s_list {
+    void *data;
+    struct s_list *next;
+} t_list;
+void mx_clear_list(t_list **list);
+t_list *mx_create_node(void *data);
+t_list *mx_get_by_index(t_list *head, int index);
+int mx_list_size(t_list *list);
+void mx_reverse_list(t_list *head);
+t_list *mx_sort_list(t_list *lst, bool (*cmp)(void *, void *));
+void mx_pop_front(t_list **head);
+void mx_pop_back(t_list **head);
+void mx_pop_index(t_list **list, int index);
+void mx_pop_node(t_list **head, t_list *node);
+void mx_push_front(t_list **list, void *data);
+void mx_push_back(t_list **list, void *data);
+
 /**
  * @brief Reads LENGTH bytes into BUFFER from SOCKET. This function is best used
  * to read 2 or more bytes.
