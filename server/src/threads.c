206a0c4ddda831b3711c4df71911efa7d256eeb6
fomgleb <fomglib@gmail.com>
2023-04-08 14:32:53 +0300

    Replace if-else with switch-case

--- server/src/threads.c
+++ server/src/threads.c
@@ -6,24 +6,25 @@ void *handle_request_thread(void *client_socket_void) {
 
     t_request client_request = receive_byte(client_socket);
 
-    if (client_request == LOGIN) {
-        handle_login(client_socket);
-    } else if (client_request == REGISTER) {
-        handle_registration(client_socket);
-    } else if (client_request == CREATE_CHAT) {
-        handle_chat_creation(client_socket);
-    } else if (client_request == GET_CHATS_I_AM_IN) {
-        handle_getting_chats(client_socket);
-    } else if (client_request == ADD_MEMBER_TO_CHAT) {
-        handle_adding_new_member_to_chat(client_socket);
-    } else if (client_request == SEND_TEXT_MESSAGE) {
-        handle_text_message_sending(client_socket);
-    } else if (client_request == GET_LAST_MESSAGES) {
-        handle_last_messages_getting(client_socket);
-    } else if (client_request == REMOVE_USER_FROM_CHAT) {
-        handle_removing_user_from_chat(client_socket);
-    } else if (client_request == GET_CHAT_MEMBERS) {
-        handle_getting_chat_members(client_socket);
+    switch (client_request) {
+    case LOGIN:
+        handle_login(client_socket); break;
+    case REGISTER:
+        handle_registration(client_socket); break;
+    case CREATE_CHAT:
+        handle_chat_creation(client_socket); break;
+    case ADD_MEMBER_TO_CHAT:
+        handle_adding_new_member_to_chat(client_socket); break;
+    case GET_CHATS_I_AM_IN:
+        handle_getting_chats(client_socket); break;
+    case SEND_TEXT_MESSAGE:
+        handle_text_message_sending(client_socket); break;
+    case GET_LAST_MESSAGES:
+        handle_last_messages_getting(client_socket); break;
+    case REMOVE_USER_FROM_CHAT:
+        handle_removing_user_from_chat(client_socket); break;
+    case GET_CHAT_MEMBERS:
+        handle_getting_chat_members(client_socket); break;
     }
 
     close(client_socket);
