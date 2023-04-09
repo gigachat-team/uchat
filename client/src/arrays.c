af13857ac4adb4db47da4a7f29d929034f207f1f
fomgleb <fomglib@gmail.com>
2023-04-09 21:08:28 +0300

    Add t_user_messages_array and a func to create it

---
+++ client/src/arrays.c
@@ -0,0 +1,9 @@
+#include "../client.h"
+
+t_user_messages_array allocate_user_messages_array(size_t size) {
+    t_user_messages_array user_messages_array = {
+        .size = size,
+        .arr = malloc(size * sizeof(t_user_message))
+    };
+    return user_messages_array;
+}
