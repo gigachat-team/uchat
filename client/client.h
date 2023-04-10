2280bb09e47586d3d49ed12940e399f3e68df720
fomgleb <fomglib@gmail.com>
2023-04-10 21:21:40 +0300

    Move common.css -> resources/style.css

--- client/client.h
+++ client/client.h
@@ -3,7 +3,7 @@
 #include "../utils/utils.h"
 #include <gtk/gtk.h>
 
-#define DEFAULT_CSS_FILE_PATH "./client/style/common.css"
+#define DEFAULT_CSS_FILE_PATH "resources/style.css"
 #define GLADE_FILE_PATH "resources/markup/uchat.glade"
 
 #define LAST_LOADING_MESSAGES_COUNT 30
