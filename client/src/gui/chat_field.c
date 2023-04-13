4e911366bdc569695857a3965858afe7ccbe47dc
fomgleb <fomglib@gmail.com>
2023-04-13 16:05:46 +0300

    Load messages when send yours

--- client/src/gui/chat_field.c
+++ client/src/gui/chat_field.c
@@ -16,21 +16,29 @@ static void create_chat_message(GtkBuilder *builder, char *message_text) {
     gtk_widget_show_all(message);
 }
 
-static void load_messages(GtkBuilder *builder, t_address *server_address, id_t chat_id) {
-    t_list_with_size messages_list = rq_get_messages_in_chat(*server_address, chat_id);
-
-    for (t_list *i = messages_list.list; i != NULL; i = i->next) {
+static void load_messages(GtkBuilder *builder, t_address *server_address, id_t chat_id, t_list_with_size *messages_in_chat) {
+    free_user_messages_list(messages_in_chat);
+    *messages_in_chat = rq_get_messages_in_chat(*server_address, chat_id);
+    for (t_list *i = messages_in_chat->list; i != NULL; i = i->next) {
         create_chat_message(builder, ((t_user_message *)i->data)->data);
     }
-
-    free_user_messages_list(&messages_list);
 }
 
-void gui_send_message(GtkBuilder *builder, t_address *server_address, id_t user_id, id_t chat_id, char *message) {
-    if (rq_send_text_message(*server_address, user_id, chat_id, message)) {
-        printf("A message sent successfully.");
+void gui_send_message(GtkBuilder *builder, t_address *server_address, id_t user_id, id_t chat_id, char *message, t_list_with_size *messages_in_chat) {
+    t_list_with_size updated_messages = rq_send_message_and_get_messages_updates(*server_address, user_id, chat_id, message, messages_in_chat);
+    for (t_list *i = updated_messages.list; i != NULL; i = i->next) {
+        t_user_message *updated_message = (t_user_message *)i->data;
+        if (i->next == NULL) {
+            create_chat_message(builder, message);
+            free(updated_message->data);
+            updated_message->data = strdup(message);
+        } else {
+            create_chat_message(builder, updated_message->data);
+        }
+        mx_push_back(&messages_in_chat->list, updated_message);
+        messages_in_chat->size++;
     }
-    create_chat_message(builder, message);
+    mx_clear_list(&updated_messages.list);
 }
 
 void gui_open_chat(t_chat_data *chat_data) {
@@ -38,7 +46,7 @@ void gui_open_chat(t_chat_data *chat_data) {
 
     clear_container(chat_data->gui_data.builder, "chat_field");
     write_label_text(chat_data->gui_data.builder, "chat_name", chat_data->chat.name);
-    load_messages(chat_data->gui_data.builder, &chat_data->gui_data.server_address, chat_data->chat.id);
+    load_messages(chat_data->gui_data.builder, &chat_data->gui_data.server_address, chat_data->chat.id, &chat_data->messages);
 
     g_signal_handlers_destroy(message_field);
     g_signal_connect(message_field, "activate", G_CALLBACK(on_send_message_clicked), chat_data);
