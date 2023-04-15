5e8628117c00625ffce3f3f1aa2369cdf8d56a35
Sergij Dekht <mistersergey055@gmail.com>
2023-04-16 00:05:56 +0200

    Add definition of set_cursor_image function and change input params for get_widget_by_id_and_apply_style one

--- client/src/gui/styles.c
+++ client/src/gui/styles.c
@@ -25,9 +25,31 @@ void apply_style_to_widget(GtkWidget *widget, const char *class_name) {
     gtk_style_context_add_class(context, class_name);
 }
 
-void get_widget_by_id_and_apply_style(t_gui_data gui_data, const char *markup_id, const char *class_name) {
-    if (!markup_id || !class_name || !gui_data.builder) return;
+void get_widget_by_id_and_apply_style(GtkBuilder *builder, const char *markup_id, const char *class_name) {
+    if (!markup_id || !class_name || !builder) return;
 
-    GtkWidget *newlogin = GTK_WIDGET(gtk_builder_get_object(gui_data.builder, markup_id));
+    GtkWidget *newlogin = GTK_WIDGET(gtk_builder_get_object(builder, markup_id));
     apply_style_to_widget(newlogin, class_name);
+}
+
+void set_cursor_image(GtkWidget *widget, const gchar *cursor_path) {
+    if (!widget || !cursor_path) return;
+
+    GdkCursor *cursor = NULL;
+    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(cursor_path, NULL);
+
+    if (!pixbuf) return;
+
+    cursor = gdk_cursor_new_from_pixbuf(gdk_display_get_default(), pixbuf, 0, 0);
+    g_object_unref(pixbuf);
+    
+    if (!cursor) return;
+    
+    GdkWindow *window = gtk_widget_get_window(widget);
+
+    if (window != NULL) {
+        gdk_window_set_cursor(window, cursor);
+    }
+
+    g_object_unref(cursor);
 }
