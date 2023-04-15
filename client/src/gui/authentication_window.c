b17d5d9378eb6f1f0fdb7ea1760fb3819b19224b
Sergij Dekht <mistersergey055@gmail.com>
2023-04-16 00:04:32 +0200

    Applying the cursor texture in a messenger window

--- client/src/gui/authentication_window.c
+++ client/src/gui/authentication_window.c
@@ -3,13 +3,16 @@
 static void open_messenger_window(GtkBuilder *gtk_builder, t_address *server_address, id_t user_id) {
     close_window(gtk_builder, AUTHENTICATION_WINDOW_ID);
     open_window(gtk_builder, MESSENGER_WINDOW_ID);
+    GtkWidget *messenger_window = GTK_WIDGET(gtk_builder_get_object(gtk_builder, MESSENGER_WINDOW_ID));
+    set_cursor_image(messenger_window, DEFAULT_CURSOR_IMAGE_PATH);
+    get_widget_by_id_and_apply_style(gtk_builder, CREATE_ROOM_ID, CSS_CLASS_NEW_CHAT_BUTTON);
     gui_render_chats_list(gtk_builder, server_address, user_id);
 }
 
 void gui_login(GtkBuilder *gtk_builder, t_address *server_address, id_t *user_id) {
     GtkWidget *error_message = get_widget(gtk_builder, ERROR_MESSAGE_LOGIN_LABEL_ID);
 
-    apply_style_to_widget(error_message, "error-message");
+    apply_style_to_widget(error_message, CSS_CLASS_ERROR_MESSAGE);
 
     char *login = get_entry_text(gtk_builder, LOGIN_ENTRY_ID);
     char *password = get_entry_text(gtk_builder, PASSWORD_ENTRY_ID);
@@ -30,7 +33,7 @@ void gui_login(GtkBuilder *gtk_builder, t_address *server_address, id_t *user_id
 
 void gui_register(GtkBuilder *gtk_builder, t_address *server_address, id_t *user_id) {
     GtkWidget *error_message = get_widget(gtk_builder, ERROR_MESSAGE_REGISTRATION_LABEL_ID);
-    apply_style_to_widget(error_message, "error-message");
+    apply_style_to_widget(error_message, CSS_CLASS_ERROR_MESSAGE);
 
     char *new_login = get_entry_text(gtk_builder, NEW_LOGIN_ENTRY_ID);
     char *new_password = get_entry_text(gtk_builder, NEW_PASSWORD_ENTRY_ID);
