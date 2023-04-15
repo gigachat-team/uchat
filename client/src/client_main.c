7e30bffe1c908d93090c5ff04b895c40d7f328be
Sergij Dekht <mistersergey055@gmail.com>
2023-04-16 00:01:40 +0200

    Applying cursos texture and removing 'white' style from labels

--- client/src/client_main.c
+++ client/src/client_main.c
@@ -37,8 +37,6 @@
 //             free_new_chat_member_data(new_chat_member_data);
 //         }
 
-
-
 int main(int argc, char **argv) {
     if (argc != 3) {
         printf(USAGE_MESSAGE);
@@ -46,18 +44,16 @@ int main(int argc, char **argv) {
     }
 
     gtk_init(&argc, &argv);
-    // load_css(DEFAULT_CSS_FILE_PATH);
+    load_css(DEFAULT_CSS_FILE_PATH);
     t_gui_data gui_data = create_gui_data(argv[1], atoi(argv[2]));
     gtk_builder_connect_signals(gui_data.builder, &gui_data);
 
-    get_widget_by_id_and_apply_style(gui_data, "Login", "white");
-    get_widget_by_id_and_apply_style(gui_data, "newlogin", "white");
-    get_widget_by_id_and_apply_style(gui_data, "newpassword", "white");
-    get_widget_by_id_and_apply_style(gui_data, "confirmpassword", "white");
-    get_widget_by_id_and_apply_style(gui_data, "new_login_entry", "white");
-
+    GtkWidget *auth_window = GTK_WIDGET(gtk_builder_get_object(gui_data.builder, AUTHENTICATION_WINDOW_ID));
     open_window(gui_data.builder, AUTHENTICATION_WINDOW_ID);
+    set_cursor_image(auth_window, DEFAULT_CURSOR_IMAGE_PATH);
+
     gtk_main();
+
     g_object_unref(gui_data.builder);
 
     return EXIT_SUCCESS;
