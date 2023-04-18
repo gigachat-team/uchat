2fbdd060dfa7c927195c7555822bc00a1c0a9e42
fomgleb <fomglib@gmail.com>
2023-04-18 18:39:20 +0300

    Add server_receivers object

---
+++ server/inc/server_receivers.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "id_and_changes_count.h"
+#include "receivers.h"
+
+t_id_and_changes_count_array receive_id_and_changes_count_array(int client_socket);
--- server/src/request_handlers/message_request_handlers.c
+++ server/src/request_handlers/message_request_handlers.c
@@ -43,58 +43,46 @@ void handle_message_sending_and_messages_updates_getting(int client_socket) {
     id_t user_id = receive_uint32(client_socket);
     id_t chat_id = receive_uint32(client_socket);
     char *data = receive_bytes(client_socket);
-    t_id_and_changes_count_array id_and_changes_count_array = create_id_and_changes_count(receive_uint32(client_socket));
-    for (size_t i = 0; i < id_and_changes_count_array.len; i++) {
-        id_and_changes_count_array.arr[i].id = receive_uint32(client_socket);
-        id_and_changes_count_array.arr[i].changes_count = receive_byte(client_socket);
-    }
+    t_id_and_changes_count_array client_messages = receive_id_and_changes_count_array(client_socket);
 
     sqlite3 *db = db_open();
     db_add_text_message(db, chat_id, user_id, data);
-    list_t *message_updates_list = db_select_message_updates(db, chat_id, &id_and_changes_count_array, true);
+    list_t *message_updates_list = db_select_message_updates(db, chat_id, &client_messages, true);
     db_close(db);
 
     send_message_updates_list(client_socket, message_updates_list);
 
     free(data);
-    free(id_and_changes_count_array.arr);
+    free(client_messages.arr);
     free_message_updates_list(message_updates_list);
 }
 
 void handle_message_deleting_and_messages_updates_getting(int client_socket) {
     uint32_t message_id = receive_uint32(client_socket);
     uint32_t chat_id = receive_uint32(client_socket);
-    t_id_and_changes_count_array id_and_changes_count_array = create_id_and_changes_count(receive_uint32(client_socket));
-    for (size_t i = 0; i < id_and_changes_count_array.len; i++) {
-        id_and_changes_count_array.arr[i].id = receive_uint32(client_socket);
-        id_and_changes_count_array.arr[i].changes_count = receive_byte(client_socket);
-    }
+    t_id_and_changes_count_array client_messages = receive_id_and_changes_count_array(client_socket);
 
     sqlite3 *db = db_open();
     db_delete_message(db, message_id);
-    list_t *message_updates_list = db_select_message_updates(db, chat_id, &id_and_changes_count_array, false);
+    list_t *message_updates_list = db_select_message_updates(db, chat_id, &client_messages, false);
     db_close(db);
 
     send_message_updates_list(client_socket, message_updates_list);
 
-    free(id_and_changes_count_array.arr);
+    free(client_messages.arr);
     free_message_updates_list(message_updates_list);
 }
 
 void handle_message_updates_getting(int client_socket) {
     id_t chat_id = receive_uint32(client_socket);
-    t_id_and_changes_count_array id_and_changes_count_array = create_id_and_changes_count(receive_uint32(client_socket));
-    for (size_t i = 0; i < id_and_changes_count_array.len; i++) {
-        id_and_changes_count_array.arr[i].id = receive_uint32(client_socket);
-        id_and_changes_count_array.arr[i].changes_count = receive_byte(client_socket);
-    }
+    t_id_and_changes_count_array client_messages = receive_id_and_changes_count_array(client_socket);
 
     sqlite3 *db = db_open();
-    list_t *message_updates_list = db_select_message_updates(db, chat_id, &id_and_changes_count_array, false);
+    list_t *message_updates_list = db_select_message_updates(db, chat_id, &client_messages, false);
     db_close(db);
 
     send_message_updates_list(client_socket, message_updates_list);
 
-    free(id_and_changes_count_array.arr);
+    free(client_messages.arr);
     free_message_updates_list(message_updates_list);
 }
