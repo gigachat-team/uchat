cdb2fa6657fcaf5b6d876c71bdba41b0272b1293
Sergij Dekht <mistersergey055@gmail.com>
2023-04-19 13:14:31 +0200

    Apply styles for widgets in the messenger window

--- client/src/gui/messenger_window/open_messenger_window.c
+++ client/src/gui/messenger_window/open_messenger_window.c
@@ -11,8 +11,17 @@ void open_messenger_window(GtkBuilder *gtk_builder, t_gui_data *data) {
     close_window(gtk_builder, AUTHENTICATION_WINDOW_ID);
     open_window(gtk_builder, MESSENGER_WINDOW_ID);
     GtkWidget *messenger_window = GTK_WIDGET(gtk_builder_get_object(gtk_builder, MESSENGER_WINDOW_ID));
+
     set_cursor_image(messenger_window, DEFAULT_CURSOR_IMAGE_PATH);
+
     get_widget_by_id_and_apply_style(gtk_builder, CREATE_ROOM_ID, CSS_CLASS_NEW_CHAT_BUTTON);
+    get_widget_by_id_and_apply_style(gtk_builder, CHAT_LISTS_WINDOW_ID, CSS_CLASS_CHATS_LIST_CONTAINER);
+    get_widget_by_id_and_apply_style(gtk_builder, CREATE_ROOM_ID, CSS_CLASS_CREATE_ROOM);
+    get_widget_by_id_and_apply_style(gtk_builder, MESSAGE_FIELD_ID, CSS_CLASS_MESSAGE_FIELD);
+    get_widget_by_id_and_apply_style(gtk_builder, CHAT_NAME_LABEL_ID, CSS_CLASS_CHAT_NAME);
+    get_widget_by_id_and_apply_style(gtk_builder, CANVAS_ID, CSS_CLASS_CANVAS);
+
     gui_render_chats_list(data->builder, &data->server_address, data->user_id);
+
     g_timeout_add(UPDATE_INTERVAL, chat_list_updater, data);
 }
