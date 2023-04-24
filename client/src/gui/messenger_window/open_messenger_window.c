bb9a2167e910af4088491485875d08bac6a926bf
fomgleb <fomglib@gmail.com>
2023-04-24 13:45:17 +0300

    Fix bug with 5 seconds "connecting..." at the beginning

--- client/src/gui/messenger_window/open_messenger_window.c
+++ client/src/gui/messenger_window/open_messenger_window.c
@@ -11,5 +11,6 @@ void open_messenger_window(GtkBuilder *gtk_builder, t_gui_data *data) {
     close_window(gtk_builder, AUTHENTICATION_WINDOW_ID);
     open_window(gtk_builder, MESSENGER_WINDOW_ID);
     apply_styles_for_messenger_window(gtk_builder);
+    gui_render_chats_list(gtk_builder, &data->server_address, data->user_id);
     g_timeout_add(UPDATE_INTERVAL, chat_list_updater, data);
 }
