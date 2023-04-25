bcd463484471bad2917399db1098144b86fbb862
fomgleb <fomglib@gmail.com>
2023-04-25 11:53:52 +0300

    Reserve deleted account name

--- server/src/request_handlers/member_request_handlers.c
+++ server/src/request_handlers/member_request_handlers.c
@@ -4,19 +4,23 @@ void handle_adding_new_member_to_chat(int client_socket) {
     id_t chat_id = receive_uint32(client_socket);
     char *member_login = receive_bytes(client_socket);
 
-    sqlite3 *db = db_open();
-    id_t user_id = db_get_user_id_by_login(db, member_login);
-    bool new_member_added = false;
-    if (user_id != 0)
-        new_member_added = db_add_new_member_to_chat(db, user_id, chat_id);
-    db_close(db);
-
-    if (user_id == 0) {
+    if (strcmp(member_login, DELETED_ACCOUNT_NAME) == 0) {
         send_byte(client_socket, SUCH_LOGIN_DOES_NOT_EXIST);
-    } else if (new_member_added) {
-        send_byte(client_socket, USER_SUCCESSFULLY_ADDED_TO_CHAT);
     } else {
-        send_byte(client_socket, SUCH_USER_IS_ALREADY_IN_CHAT);
+        sqlite3 *db = db_open();
+        id_t user_id = db_get_user_id_by_login(db, member_login);
+        bool new_member_added = false;
+        if (user_id != 0)
+            new_member_added = db_add_new_member_to_chat(db, user_id, chat_id);
+        db_close(db);
+
+        if (user_id == 0) {
+            send_byte(client_socket, SUCH_LOGIN_DOES_NOT_EXIST);
+        } else if (new_member_added) {
+            send_byte(client_socket, USER_SUCCESSFULLY_ADDED_TO_CHAT);
+        } else {
+            send_byte(client_socket, SUCH_USER_IS_ALREADY_IN_CHAT);
+        }
     }
 
     free(member_login);
--- server/src/request_handlers/user_request_handlers.c
+++ server/src/request_handlers/user_request_handlers.c
@@ -4,17 +4,21 @@ void handle_registration(int client_socket) {
     char *login = receive_bytes(client_socket);
     char *password = receive_bytes(client_socket);
 
-    sqlite3 *db = db_open();
-    id_t user_id = db_create_user(db, login, password);
-    db_close(db);
-
-    if (user_id != 0) {
-        t_package package = create_package(2);
-        pack_byte(SUCCESSFUL_REGISTRATION, &package);
-        pack_uint32(user_id, &package);
-        send_and_free_package(client_socket, &package);
-    } else {
+    if (strcmp(login, DELETED_ACCOUNT_NAME) == 0) {
         send_byte(client_socket, SUCH_LOGIN_ALREADY_EXISTS);
+    } else {
+        sqlite3 *db = db_open();
+        id_t user_id = db_create_user(db, login, password);
+        db_close(db);
+
+        if (user_id != 0) {
+            t_package package = create_package(2);
+            pack_byte(SUCCESSFUL_REGISTRATION, &package);
+            pack_uint32(user_id, &package);
+            send_and_free_package(client_socket, &package);
+        } else {
+            send_byte(client_socket, SUCH_LOGIN_ALREADY_EXISTS);
+        }
     }
 
     free(login);
@@ -25,41 +29,49 @@ void handle_login(int client_socket) {
     char *login = receive_bytes(client_socket);
     char *password = receive_bytes(client_socket);
 
-    sqlite3 *db = db_open();
-    id_t user_id = db_get_user_id_by_login(db, login);
-    char *found_password = db_get_password_by_id(db, user_id);
-    db_close(db);
-
-    if (found_password != NULL) {
-        if (strcmp(password, found_password) == 0) {
-            t_package package = create_package(2);
-            pack_byte(SUCCESSFUL_LOGIN, &package);
-            pack_uint32(user_id, &package);
-            send_and_free_package(client_socket, &package);
-        } else {
-            send_byte(client_socket, WRONG_PASSWORD);
-        }
-    } else {
+    if (strcmp(login, DELETED_ACCOUNT_NAME) == 0) {
         send_byte(client_socket, SUCH_LOGIN_DOES_NOT_EXIST);
+    } else {
+        sqlite3 *db = db_open();
+        id_t user_id = db_get_user_id_by_login(db, login);
+        char *found_password = db_get_password_by_id(db, user_id);
+        db_close(db);
+
+        if (found_password != NULL) {
+            if (strcmp(password, found_password) == 0) {
+                t_package package = create_package(2);
+                pack_byte(SUCCESSFUL_LOGIN, &package);
+                pack_uint32(user_id, &package);
+                send_and_free_package(client_socket, &package);
+            } else {
+                send_byte(client_socket, WRONG_PASSWORD);
+            }
+        } else {
+            send_byte(client_socket, SUCH_LOGIN_DOES_NOT_EXIST);
+        }
+        free(found_password);
     }
 
     free(login);
     free(password);
-    free(found_password);
 }
 
 void handle_login_changing(int client_socket) {
     uint32_t user_id = receive_uint32(client_socket);
     char *new_login = receive_bytes(client_socket);
 
-    sqlite3 *db = db_open();
-    bool login_already_exists = db_change_login(db, user_id, new_login);
-    db_close(db);
-
-    if (login_already_exists) {
+    if (strcmp(new_login, DELETED_ACCOUNT_NAME) == 0) {
         send_byte(client_socket, SUCH_LOGIN_ALREADY_EXISTS);
     } else {
-        send_byte(client_socket, LOGIN_SUCCESSFULLY_CHANGED);
+        sqlite3 *db = db_open();
+        bool login_already_exists = db_change_login(db, user_id, new_login);
+        db_close(db);
+
+        if (login_already_exists) {
+            send_byte(client_socket, SUCH_LOGIN_ALREADY_EXISTS);
+        } else {
+            send_byte(client_socket, LOGIN_SUCCESSFULLY_CHANGED);
+        }
     }
 
     free(new_login);
