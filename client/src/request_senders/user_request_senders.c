ef399222f0ebdee02fb260cfa42d9cfab999417a
fomgleb <fomglib@gmail.com>
2023-04-25 12:34:47 +0300

    Fix bug: forgot to send request byte

--- client/src/request_senders/user_request_senders.c
+++ client/src/request_senders/user_request_senders.c
@@ -24,7 +24,8 @@ t_state_code rq_change_login(t_address *server_address, id_t user_id, char *new_
     int client_socket = create_and_connect_socket(server_address);
     if (client_socket == -1) return CONNECTION_REFUSED;
 
-    t_package package = create_package(2);
+    t_package package = create_package(3);
+    pack_byte(CHANGE_LOGIN, &package);
     pack_uint32(user_id, &package);
     pack_bytes(new_login, &package);
     send_and_free_package(client_socket, &package);
