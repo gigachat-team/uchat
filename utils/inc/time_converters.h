a941bd5bc1f0c60f8edb0595acbff232d7ac498c
fomgleb <fomglib@gmail.com>
2023-04-26 09:17:39 +0300

    Fix date string bug

--- utils/inc/time_converters.h
+++ utils/inc/time_converters.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 #define DEFAULT_TIME_FORMAT "%2m-%2d %2H:%2M"
-#define DEFAULT_TIME_FORMAT_LEN 11
+#define DEFAULT_TIME_FORMAT_LEN 12
 
 struct tm str_to_tm(char *datetime_str, char *datetime_format);
 struct tm utc_to_localtime(struct tm utc);
