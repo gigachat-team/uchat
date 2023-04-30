11e153e8f2a63aa7d7f9ecce4800088b22240229
fomgleb <fomglib@gmail.com>
2023-04-30 20:38:09 +0300

    Add read_three_files function

--- utils/inc/file.h
+++ utils/inc/file.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "str.h"
 #include <stdio.h>
 
 void write_to_file(char *filename, char *text);
 void read_from_file(char *filename, size_t max_len, char *buffer);
+t_string read_three_files(char *filename1, char *filename2, char *filename3);
